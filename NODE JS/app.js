35b9ed15f8698f679311d862227838b36ef82200
supriya <supriyabarik116@gmail.com>
2023-12-11 20:54:05 +0530

    form and inputs using middlewares

--- NODE JS/app.js
+++ NODE JS/app.js
@@ -2,13 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log("In the middleware!");
-    next();
-})
-app.use((req,res,next) => {
-    console.log("In the middleware 2!");
-    res.send("<h1> Hello to node js</h1>");
-})
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req,res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button>Add Product</button></form>')
+});
+
+app.post('/product', (req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req,res,next) => {
+    res.send("<h1> Hello to node js </h1>");
+});
 
 app.listen(3000);
