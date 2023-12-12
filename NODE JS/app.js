1842bdc2a961f0e5d0b40636f898ed88e9be9d86
supriya <supriyabarik116@gmail.com>
2023-12-12 09:41:13 +0530

    Routers and filters

--- NODE JS/app.js
+++ NODE JS/app.js
@@ -4,19 +4,16 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req,res,next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button>Add Product</button></form>')
-});
+app.use('/admin', adminRoute);
+app.use('/shop', shopRoute);
 
-app.post('/product', (req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req,res,next) => {
-    res.send("<h1> Hello to node js </h1>");
-});
+app.use((req,res,next) => {
+    res.statusCode(404).send('<h1> Page Not Found </h1>');
+})
 
 app.listen(3000);
