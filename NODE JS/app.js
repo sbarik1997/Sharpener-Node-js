dd07943ff477528b8adfc7974391a7cfa179445d
supriya <supriyabarik116@gmail.com>
2023-12-12 17:26:44 +0530

    chat app

--- NODE JS/app.js
+++ NODE JS/app.js
@@ -4,16 +4,46 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-const adminRoute = require('./routes/admin');
-const shopRoute = require('./routes/shop');
+const fs = require('fs');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/admin', adminRoute);
-app.use('/shop', shopRoute);
 
-app.use((req,res,next) => {
-    res.statusCode(404).send('<h1> Page Not Found </h1>');
-})
+app.get('/login', (req,res,next) => {
+    res.send(`<form onSubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/login" method="POST"><input type="text" id="username" placeholder="username" name="username"><button>login</button></form>`)
+});
 
-app.listen(3000);
+app.post('/login', (req,res,next) => {
+    res.redirect('/');
+});
+
+app.get('/', (req,res) => {
+
+    fs.readFile('messege.txt', (err,data) => {
+        if(err){
+            data = 'No chat exist!'
+        }
+
+        res.send(
+            `${data}<form onSubmit="document.getElementById('username').value = localStorage.getItem('username')" action="/" method="POST">
+            <input type="text" id="messege" name="messege">
+            <input type="hidden" id="username" name="username">
+            <button>Send</button>
+            </form>`)
+        });
+    })
+
+    
+
+app.post('/', (req,res) => {
+    // console.log(req.data.messege);
+    // console.log(req.data.username);
+
+    fs.writeFile('messege.txt', `${req.body.username} : ${req.body.messege}`, {flag: 'a'}, (err) => {
+        err? console.log(err) : res.redirect('/');
+    });
+});
+
+
+app.listen(5000);
+
